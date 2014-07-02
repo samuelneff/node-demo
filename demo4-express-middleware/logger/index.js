6c46c3e6dca106dda9081a5046f0f64387ba4f00
Khiem Tong <tong.khiem@gmail.com>
2014-07-02 15:47:52 -0400

    Update readme

--- demo4-express-middleware/logger/index.js
+++ demo4-express-middleware/logger/index.js
@@ -1,8 +1,11 @@
 // This file is loaded by default by the module resolver 
 // unless overridden by package.json
 
+// all middleware components take these function parameters
 function logger(req, res, next) {
 	console.log("%s %s", req.method, req.url);
+
+	// next has to be called explicit otherwise you stall the whole stack
 	next();
 }
 
